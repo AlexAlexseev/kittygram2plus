f5de1f698341a99a153f46fd3258cd53f1701263
Алексеев Александр <alexxseev@yandex.ru>
2023-05-05 11:00:16 +0500

    add

--- kittygram2plus/settings.py
+++ kittygram2plus/settings.py
@@ -128,13 +128,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated', 
+        'rest_framework.permissions.IsAuthenticated',
     ],
 
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
 }
+
 
 SIMPLE_JWT = {
    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
